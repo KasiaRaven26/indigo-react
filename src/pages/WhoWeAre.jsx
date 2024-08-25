c0b90c0ba28be714a4c2899fcbc3744f0f135606
KasiaRaven26 <rejven777@gmail.com>
2024-08-25 19:12:13 +0100

    newcommits

--- src/pages/WhoWeAre.jsx
+++ src/pages/WhoWeAre.jsx
@@ -6,23 +6,21 @@ export function WhoWeArePage() {
   return (
     <div>
       <Header></Header>
-      <main>
+      <main className={classes.whoWeAreMain}>
         <section className={classes.section1}>
           <h1>WHO WE ARE</h1>
-          <h3>
-            INDIGO was established in 2021, seizes the opportune intersection of
-            location and timing for real estate investment.
-          </h3>
-          <h3>
-            Our mission is to streamline the investment journey for potential
+          <p>
+            INDIGO was established in <span>2021</span>, seizes the opportune
+            intersection of location and timing for real estate investment. Our
+            mission is to streamline the investment journey for potential
             investors, saving them time, research efforts and the disappointment
             that can accompany such pursuits.
-          </h3>
-          <h3>
-            With 20 years of accumulated local knowledge and expertise our
-            founder guides you towards informed decisions and profitable
-            ventures.
-          </h3>
+          </p>
+          <p>
+            With <span>20 years</span> of accumulated local knowledge and
+            expertise our founder guides you towards informed decisions and
+            profitable ventures.
+          </p>
           <Button to="/">MORE</Button>
         </section>
         <section className={classes.section2}>
@@ -38,21 +36,21 @@ export function WhoWeArePage() {
             />
           </div>
           <div className={classes.paragraphContainer}>
-            <h3>
-              With more than 25 years of extensive local expertise spanning
-              diverse business sectors, I have cultivated a deep understanding
-              of the investment landscape. 
-            </h3>
-            <h3>
-              Over the last 18 years, my focus has extended to encompass real
-              estate.
-            </h3>
+            <p>
+              With more than <span>25 years</span> of extensive local expertise
+              spanning diverse business sectors, I have cultivated a deep
+              understanding of the investment landscape. 
+            </p>
+            <p>
+              Over the last <span>18 years</span>, my focus has extended to
+              encompass real estate.
+            </p>
              
-            <h3>
-              Having served as CEO in over 10 companies within this
+            <p>
+              Having served as CEO in over <span>10 companies</span> within this
               quarter-century timeframe, I bring a wealth of knowledge and
               experience to the table.
-            </h3>
+            </p>
           </div>
         </section>
       </main>
