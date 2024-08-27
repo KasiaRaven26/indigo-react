9782b4cc04abac1d7edccbaef8b2ad237b268f7e
KasiaRaven26 <rejven777@gmail.com>
2024-08-27 23:30:41 +0100

    build

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { SignInPage } from "./pages/SignInPage";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/projects" element={<Projects />} />
